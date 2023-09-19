076d49af1254f3ed2257ad490886d6068b691e7b
PaulaGSUni <maria.guerrero05@usa.edu.co>
2023-09-19 18:14:59 -0500

    operaciones

--- ejemploetl.py
+++ ejemploetl.py
@@ -1,2 +1,31 @@
-install pandas as pd 
+import pandas as pd
+archivo_origen = "g1etl\data.csv"
+
+# Leer los datos desde el archivo CSV y especificar que la primera fila es el encabezado
+datos = pd.read_csv(archivo_origen, delimiter=';', header=0)
+
+# Verificar los primeros registros
+print(datos.head())
+print(datos.columns)
+# Agregar una nueva columna 'suma' que contenga la suma de dos columnas existentes
+# Asumiendo que las columnas se llaman 'columna1' y 'columna2' en tu archivo CSV
+datos['suma'] = datos['columna1'] + datos['columna2']
+datos['resta'] = datos['columna1'] - datos['columna2']
+datos['multiplicacion'] = datos['columna1'] * datos['columna2']
+
+
+
+
+# Verificar los cambios
+print(datos.head())
+
+# Ruta al archivo CSV de destino
+archivo_destino = "datos_transformados.csv"
+
+# Guardar los datos transformados en un nuevo archivo CSV
+datos.to_csv(archivo_destino, index=False)
+
+# Verificar que se haya guardado correctamente
+print(f"Los datos transformados se han guardado en {archivo_destino}")
+
 
